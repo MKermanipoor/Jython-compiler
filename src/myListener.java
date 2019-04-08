7e52c9ea1428e7a078a7142ffdab0fc498b21d64
MKP <masoud.kermanipoor@gmail.com>
2019-04-08 20:01:15 +0430

    find and print method number

--- src/myListener.java
+++ src/myListener.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 
 
 import gen.grmBaseListener;
@@ -11,21 +6,20 @@ import gen.grmBaseListener;
 import gen.grmParser;
 import org.antlr.v4.runtime.misc.NotNull;
 
-/**
- *
- * @author Amin
- */
 public class myListener extends grmBaseListener {
 
-//    @Override
-//    public void exitMessage(@NotNull grmParser.MessageContext ctx) {
-//        System.out.println("+ " + ctx.getChild(1).getText());
-//    }
-
+    private int method_defune_number = 0;
 
     @Override
     public void exitProgram(grmParser.ProgramContext ctx) {
-        System.out.println(ctx.children.size());
         System.out.println("+ " + ctx.getChild(1).getText());
+        System.out.println("method define number : " + method_defune_number);
+    }
+
+    @Override
+    public void exitMethodDec(grmParser.MethodDecContext ctx) {
+        System.out.print("method name : ");
+        System.out.println(ctx.getChild(2).getText());
+        method_defune_number ++;
     }
 }
